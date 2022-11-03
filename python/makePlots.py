81dc32e03552623e38d14e50bc280da11ce11ece
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-11-03 12:01:54 -0500

    Improve labels and ticks for limit plot.

--- python/makePlots.py
+++ python/makePlots.py
@@ -1,6 +1,7 @@
 # makePlots.py
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 import tools
 
 def plot(plot_dir, plot_name, inputs, info):
@@ -13,16 +14,22 @@ def plot(plot_dir, plot_name, inputs, info):
     fig, ax = plt.subplots(figsize=(6, 6))
     
     for key in inputs:
-        label   = key.replace('_', ' ')
         data    = inputs[key]["data"]
+        label   = inputs[key]["label"]
         color   = inputs[key]["color"]
         x_vals, y_vals = tools.getXYVals(data)
         plt.plot(x_vals, y_vals, label=label, color=color)
     
-    ax.legend()
-    ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    legend_font_size = 12
+    ax.legend(loc='upper right', prop={'size': legend_font_size})
+    ax.set_title(title,     fontsize=20)
+    ax.set_xlabel(x_label,  fontsize=16)
+    ax.set_ylabel(y_label,  fontsize=16)
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.tick_params(axis='both', which='both', bottom=True, top=True, left=True, right=True, direction="in")
+    ax.tick_params(axis='both', which='major', labelsize=16, length=8)
+    ax.tick_params(axis='both', which='minor', labelsize=8,  length=4)
     plt.savefig(output_name)
 
 def makePlots():
@@ -34,12 +41,13 @@ def makePlots():
     inputs = {}
     inputs["ATLAS_Soft_2l"] = {}
     inputs["ATLAS_Soft_2l"]["csv"]      = "data/HEPData-ins1767649-v5-Figure_16a_Observed.csv"
+    inputs["ATLAS_Soft_2l"]["label"]    = "ATLAS Soft 2l (Observed)"
     inputs["ATLAS_Soft_2l"]["color"]    = "xkcd:cherry red"
 
     info = {}
     info["title"]   = "TSlepSlep Limits"
-    info["x_label"] = "X"
-    info["y_label"] = "Y"
+    info["x_label"] = r"$m \left(\tilde{\ell}\right)$ [GeV]"
+    info["y_label"] = r"$\Delta m \left(\tilde{\ell}, \tilde{\chi}_{1}^{0}\right)$ [GeV]"
     
     # load data from csv file
     for key in inputs:
