70b41b11b588d80a7f10a3011c2c3269c8b24c4c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-11-03 10:26:39 -0500

    Add plot labels.

--- python/makePlots.py
+++ python/makePlots.py
@@ -3,29 +3,43 @@
 import matplotlib.pyplot as plt
 import tools
 
-def plot(plot_dir, plot_name, inputs):
+def plot(plot_dir, plot_name, inputs, info):
+    output_name = "{0}/{1}.pdf".format(plot_dir, plot_name)
+    
+    title   = info["title"]
+    x_label = info["x_label"]
+    y_label = info["y_label"]
+    
     for key in inputs:
         data = inputs[key]["data"]
         x_vals, y_vals = tools.getXYVals(data)
         plt.plot(x_vals, y_vals)
-    output_name = "{0}/{1}.pdf".format(plot_dir, plot_name)
+    
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.savefig(output_name)
 
 def makePlots():
     print("Yeah baby, let's go!")
     plot_dir    = "plots"
-    plot_name   = "TSlepSlep_Run2"
+    plot_name   = "TSlepSlep_Limits"
     
     inputs = {}
     inputs["ATLAS_TSlepSlep_Soft2l"] = {}
     inputs["ATLAS_TSlepSlep_Soft2l"]["csv"] = "data/HEPData-ins1767649-v5-Figure_16a_Observed.csv"
+
+    info = {}
+    info["title"]   = "TSlepSlep Limits"
+    info["x_label"] = "X"
+    info["y_label"] = "Y"
     
     for key in inputs:
         inputs[key]["data"] = tools.getData(inputs[key]["csv"]) 
         inputs[key]["data"] = tools.getCleanData(inputs[key]["data"])
     
     tools.makeDir(plot_dir)
-    plot(plot_dir, plot_name, inputs)
+    plot(plot_dir, plot_name, inputs, info)
 
 def main():
     makePlots()
