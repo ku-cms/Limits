a686d3ee7ebcc66b3cf3b1ae017f74808d6499b7
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-11-03 10:44:14 -0500

    Add legend, label, and color.

--- python/makePlots.py
+++ python/makePlots.py
@@ -9,15 +9,20 @@ def plot(plot_dir, plot_name, inputs, info):
     title   = info["title"]
     x_label = info["x_label"]
     y_label = info["y_label"]
+
+    fig, ax = plt.subplots(figsize=(6, 6))
     
     for key in inputs:
-        data = inputs[key]["data"]
+        label   = key.replace('_', ' ')
+        data    = inputs[key]["data"]
+        color   = inputs[key]["color"]
         x_vals, y_vals = tools.getXYVals(data)
-        plt.plot(x_vals, y_vals)
+        plt.plot(x_vals, y_vals, label=label, color=color)
     
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+    ax.legend()
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
     plt.savefig(output_name)
 
 def makePlots():
@@ -25,15 +30,18 @@ def makePlots():
     plot_dir    = "plots"
     plot_name   = "TSlepSlep_Limits"
     
+    # TSlepSlep
     inputs = {}
-    inputs["ATLAS_TSlepSlep_Soft2l"] = {}
-    inputs["ATLAS_TSlepSlep_Soft2l"]["csv"] = "data/HEPData-ins1767649-v5-Figure_16a_Observed.csv"
+    inputs["ATLAS_Soft_2l"] = {}
+    inputs["ATLAS_Soft_2l"]["csv"]      = "data/HEPData-ins1767649-v5-Figure_16a_Observed.csv"
+    inputs["ATLAS_Soft_2l"]["color"]    = "xkcd:cherry red"
 
     info = {}
     info["title"]   = "TSlepSlep Limits"
     info["x_label"] = "X"
     info["y_label"] = "Y"
     
+    # load data from csv file
     for key in inputs:
         inputs[key]["data"] = tools.getData(inputs[key]["csv"]) 
         inputs[key]["data"] = tools.getCleanData(inputs[key]["data"])
